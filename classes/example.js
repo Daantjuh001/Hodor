53fd7a91133e737f1b01575d13f52c2bdaf668ff
Jasper van M <jasper@vanm.info>
2014-03-27 09:58:25 +0100

    JS Classes whoop

--- classes/example.js
+++ classes/example.js
@@ -1,7 +1,56 @@
-function Example(paramenter1, parameter2) {
-	this.variable = parameter1;
-}
+function jsObj() {
 
-Example.prototype.doStuff = function() {
-	alert("Hallooo");
+    this.m_public = 'je moer'; //public variable
+
+    this.o_griffins; //public variable
+
+
+
+    this.doGriffins = doGriffins; // public method
+
+    this.doSayItOutLoud = doSayItOutLoud; // public method
+
+    this.getPrivateVariable = getPrivateVariable; // public method
+
+
+
+    var m_private = 'je var'; // private variable
+
+
+    function doSayItOutLoud(m_var_fp) { // this method is public because it has a "this." pointer
+
+        this.m_public = m_var_fp;
+
+        if (confirm(m_var_fp + ' is public knowledge. Do you wish to save it?')) {
+
+            setPrivateVariable(m_var_fp);
+
+        }
+
+    }
+
+
+
+    function doGriffins() {
+
+        this.o_griffins = new familyObj();
+
+    }
+
+
+
+    function getPrivateVariable() { //this method is public because it has a "this." pointer
+
+        alert('The private variable is ' + m_private);
+
+    }
+
+
+
+    function setPrivateVariable(m_var_fp) { //this method is private because it has no "this." pointer
+
+        m_private = m_var_fp;
+
+    }
+
 }
